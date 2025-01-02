11d1ba181edd0d1b0ae5ca53519bb99888f4e05d
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-01-02 16:46:24 +0500

    Added company.controller.js and updated model file

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const companySchema=new mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     description:{
         type:String,
@@ -23,4 +24,4 @@ const companySchema=new mongoose.Schema({
         required:true
     }
 },{timestamps:true});
-export const Job=mongoose.model("Job",JobSchema)
+export const Company=mongoose.model("Company",companySchema)
