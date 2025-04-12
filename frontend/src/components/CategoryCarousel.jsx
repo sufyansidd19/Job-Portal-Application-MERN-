162ec327521534195ad799388c8a117b53e29852
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-04-12 17:08:28 +0500

    Updated Home Components

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
+import { Button } from './ui/button'
 
 
-const category=[
+const category = [
     "Frontend Developer",
     "Backend Developer",
     "Graphic Designer",
@@ -12,9 +14,26 @@ const category=[
     "Flutter Developer"
 ]
 const CategoryCarousel = () => {
-  return (
-    <div>CategoryCarousel</div>
-  )
+    // const dispatch = useDispatch();
+    // const navigate = useNavigate();
+
+    return (
+        <div>
+            <Carousel className="w-full max-w-xl mx-auto my-20">
+                <CarouselContent>
+                    {
+                        category.map((cat, index) => (
+                            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                                <Button variant="outline" className="rounded-full bg-black text-white">{cat}</Button>
+                            </CarouselItem>
+                        ))
+                    }
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
+    )
 }
 
 export default CategoryCarousel
