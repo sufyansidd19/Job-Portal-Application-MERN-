bd663a5f9337d590b02b3b07453ec12eb8521b2f
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-04-14 17:21:03 +0500

    Building Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
+import Browse from './components/Browse'
 
 
 
@@ -25,6 +26,10 @@ const appRouter=createBrowserRouter([
   {
     path:"/jobs",
     element:<Jobs/>
+  },
+  {
+    path:"/browse",
+    element:<Browse/>
   }
 
 ])
---
+++ frontend/src/components/Browse.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './shared/Navbar'
+
+const randomJobs=[1,2,3]
+const Browse = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div>
+            {/* <h1>Search Results ({randomJobs.length})</h1>
+            {
+                randomJobs.map((item,index)) */}
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Browse
