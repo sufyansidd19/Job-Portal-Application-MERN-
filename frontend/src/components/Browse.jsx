4da1d156467a7d0256fa1ee0dadc6f782e47ca50
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-04-15 14:00:46 +0500

    Line

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import Job from './Job'
 
 const randomJobs=[1,2,3]
 const Browse = () => {
   return (
     <div>
         <Navbar/>
-        <div>
-            {/* <h1>Search Results ({randomJobs.length})</h1>
+        <div className='max-w-7xl mx-auto my-10'>
+            <h1>Search Results ({randomJobs.length})</h1>
+            <div className="grid grid-cols-3 gap-4">
             {
-                randomJobs.map((item,index)) */}
+                randomJobs.map((item,index))=>{
+                  return(
+                    </Job>
+                  )
+                }
             }
+            </div>
+           
         </div>
     </div>
   )
