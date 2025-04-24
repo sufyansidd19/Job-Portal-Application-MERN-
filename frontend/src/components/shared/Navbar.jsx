8f7e47e2a26de5b7a97a83a38ec5ca7519764f7d
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-04-24 13:58:54 +0500

    Logout Functionality

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -3,12 +3,31 @@ import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { Button } from '../ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import { LogOut, User2 } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'sonner';
+import axios from 'axios';
+import { USER_API_END_POINT } from '../utils/constant';
+import { setUser } from '@/redux/authSlice';
 
 const Navbar = () => {
     const {user}=useSelector(store=>store.auth);
-    
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+
+    const logoutHandler=async()=>{
+        try {
+            const res=await axios.get(`${USER_API_END_POINT}/logout`,{withCredentials:true});
+            if(res.data.success){
+                dispatch(setUser(null));
+                navigate("/");
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message)
+        }
+    }
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -54,7 +73,7 @@ const Navbar = () => {
                                             </div>
                                             <div className="flex w-fit items-center gap-2 cursor-pointer">
                                                 <LogOut />
-                                                <Button variant="link">Logout</Button>
+                                                <Button variant="link" className="cursor-pointer" onClick={logoutHandler}>Logout</Button>
                                             </div>
 
                                         </div>
