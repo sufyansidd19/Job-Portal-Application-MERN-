139e9594ac68c0b8354366eed327c211bc82fb74
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-05-10 23:17:16 +0500

    Jobs Modification

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -6,46 +6,47 @@ import { Badge } from './ui/badge'
 import { useNavigate } from 'react-router-dom'
 
 const Job = ({job}) => {
-    const navigate=useNavigate();
-    // const jobId="fdfdfdrererre"
-    const daysAgoFunction=(mongodbTime)=>{
-        const createdAt=new Date(mongodbTime);
-        const currentTime=new Date();
-        const timeDifference=currentTime-createdAt;
-        return Math.floor(timeDifference/(1000*24*60*60))
+    const navigate = useNavigate();
+    // const jobId = "lsekdhjgdsnfvsdkjf";
+
+    const daysAgoFunction = (mongodbTime) => {
+        const createdAt = new Date(mongodbTime);
+        const currentTime = new Date();
+        const timeDifference = currentTime - createdAt;
+        return Math.floor(timeDifference/(1000*24*60*60));
     }
- 
+    
     return (
         <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
             <div className='flex items-center justify-between'>
-                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt===0 ? "Today":`${daysAgoFunction(job?.createdAt)} days ago`)}</p>
+                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
                 <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
             </div>
 
-            <div className="flex items-center gap-2 my-2">
-                <Button>
+            <div className='flex items-center gap-2 my-2'>
+                <Button className="p-6" variant="outline" size="icon">
                     <Avatar>
-                        <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsDcwXQ2w9u4FMlEf_MMgtV0UGOblUKItSPQ&s" />
+                        <AvatarImage src={job?.company?.logo} />
                     </Avatar>
                 </Button>
                 <div>
                     <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
-                    <p className='text-sm text-gray-500'>Pakistan</p>
+                    <p className='text-sm text-gray-500'>India</p>
                 </div>
             </div>
+
             <div>
                 <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
-                <p className='text-sm text-gray-'>{job?.description}</p>
+                <p className='text-sm text-gray-600'>{job?.description}</p>
             </div>
-            <div className='flex items-center gap-2 mt-4 '>
+            <div className='flex items-center gap-2 mt-4'>
                 <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
                 <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
-                <Badge className={'text-[#7209B7] font-bold'} variant="ghost">{job?.salary} LPA</Badge>
+                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary}LPA</Badge>
             </div>
-
-            <div className="flex items-center gap-4 mt-4">
-                <Button onClick={()=>navigate(`/description/${job?._id}`)} variant="outline" className='cursor-pointer'>Details</Button>
-                <Button className='bg-[#7209b7] text-white'>Save For Later</Button>
+            <div className='flex items-center gap-4 mt-4'>
+                <Button onClick={()=> navigate(`/description/${job?._id}`)} variant="outline">Details</Button>
+                <Button className="bg-[#7209b7]">Save For Later</Button>
             </div>
         </div>
     )
