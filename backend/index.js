ceeff2a9951737104165fca4b88d911d6b1d1178
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-05-14 23:25:13 +0500

    final

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const corsOption={
     origin: [
         'http://localhost:5173',
         'https://job-portal-application-mern-wide.vercel.app',
-        'https://f3d3-202-47-53-22.ngrok-free.app'
+        'https://43b7-202-47-53-22.ngrok-free.app'
         // 'https://amazed-modern-gull.ngrok-free.app'
         // 'https://job-portal-application-mern-production.up.railway.app'
     ],
--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -2,7 +2,6 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/
 
 // const API_BASE_URL="https://8630-111-68-109-251.ngrok-free.app"
 
-// 
 export const USER_API_END_POINT = `${API_BASE_URL}/user`;
 export const JOB_API_END_POINT = `${API_BASE_URL}/job`;
 export const APPLICATION_API_END_POINT = `${API_BASE_URL}/application`;
